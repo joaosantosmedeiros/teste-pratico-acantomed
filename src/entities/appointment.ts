d7fabe10ec8851877ec0c5d63fe26969ea3754af
joaosantosmedeiros <jopesame@gmail.com>
2024-04-13 18:44:31 -0300

    refactor: remove logic from appointment domain class

--- src/entities/appointment.ts
+++ src/entities/appointment.ts
@@ -18,29 +18,13 @@ export class Appointment {
     const { date, type, pacientId, id } = props;
 
     if (id) {
-      const idIsValid = validateUUID(id);
-      if (!idIsValid) {
-        throw new Error('Id must be valid.');
-      }
       this.id = id;
     } else {
       this.id = randomUUID();
     }
 
-    const pacientIdIsValid = validateUUID(pacientId);
-    if (!pacientIdIsValid) {
-      throw new Error('Pacient id must be valid.');
-    }
     this.pacientId = pacientId;
-
-    if (type.length < 5) {
-      throw new Error('Type must be greater or equal than 5 characters.');
-    }
     this.type = type;
-
-    if (date < new Date()) {
-      throw new Error('Date must be future.');
-    }
     this.date = date;
   }
 }
