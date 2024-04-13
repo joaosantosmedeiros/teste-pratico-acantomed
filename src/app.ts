fab942e9166cad5e3e1b919bffb3cf4681ca1d51
joaosantosmedeiros <jopesame@gmail.com>
2024-04-13 17:30:23 -0300

    refactor: move main file directory

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,15 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import routes from './routes/index';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
+
+app.use(express.json());
+
+app.use('/', routes);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
