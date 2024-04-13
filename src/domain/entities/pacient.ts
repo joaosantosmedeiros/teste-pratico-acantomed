17b0c9ed6caf54262cc95dc2d1353b7c2993b3fc
joaosantosmedeiros <jopesame@gmail.com>
2024-04-13 14:59:42 -0300

    refactor: remove validation code from domain entity

--- src/domain/entities/pacient.ts
+++ src/domain/entities/pacient.ts
@@ -21,35 +21,14 @@ export class Pacient {
   constructor(props: PacientProps) {
     const { id, name, email, cpf, password } = props;
 
-    if (name.length < 5) {
-      throw new Error('Name must be greater or equal than 5 characters.');
-    }
     this.name = name;
-
-    if (password.length < 5) {
-      throw new Error('Password must be greater or equal than 5 characters.');
-    }
     this.password = password;
-
-    const emailIsValid = validateEmail(email);
-    if (!emailIsValid) {
-      throw new Error('Email must be valid.');
-    }
     this.email = email;
-
-    const cpfIsValid = validateCpf(cpf);
-    if (!cpfIsValid) {
-      throw new Error('CPF must be valid.');
-    }
     this.cpf = cpf;
 
     if (!id) {
       this.id = randomUUID();
     } else {
-      const idIsValid = validateUUID(id);
-      if (!idIsValid) {
-        throw new Error('Id must be valid.');
-      }
       this.id = id;
     }
   }
--- test/entities/pacient.spec.ts
+++ test/entities/pacient.spec.ts
@@ -2,64 +2,6 @@ import { it, describe, expect } from '@jest/globals';
 import { Pacient } from '../../src/domain/entities/pacient';
 
 describe('Pacient entity tests', () => {
-  it('should not create a pacient with an invalid cpf', () => {
-    expect(() => {
-      new Pacient({
-        cpf: '77777777777',
-        email: 'mail@mail.com',
-        name: 'any_name',
-        password: '12345',
-      });
-    }).toThrow(new Error('CPF must be valid.'));
-  });
-
-  it('should not create a pacient with an invalid email', () => {
-    expect(() => {
-      new Pacient({
-        cpf: '57926458090',
-        email: 'mailmail.com',
-        name: 'any_name',
-        password: '12345',
-      });
-    }).toThrow(new Error('Email must be valid.'));
-  });
-
-  it('should not create a pacient with an invalid name', () => {
-    expect(() => {
-      new Pacient({
-        cpf: '57926458090',
-        email: 'mail@mail.com',
-        name: 'name',
-        password: '12345',
-      });
-    }).toThrow(new Error('Name must be greater or equal than 5 characters.'));
-  });
-
-  it('should not create a pacient with an invalid id', () => {
-    expect(() => {
-      new Pacient({
-        id: '231i2312',
-        cpf: '57926458090',
-        email: 'mail@mail.com',
-        name: 'any_name',
-        password: '12345',
-      });
-    }).toThrow('Id must be valid.');
-  });
-
-  it('should not create a pacient with an invalid password', () => {
-    expect(() => {
-      new Pacient({
-        cpf: '57926458090',
-        email: 'mail@mail.com',
-        name: 'any_name',
-        password: '1234',
-      });
-    }).toThrow(
-      new Error('Password must be greater or equal than 5 characters.'),
-    );
-  });
-
   it('should generate a random id if no id is passed', () => {
     const pacient = new Pacient({
       cpf: '57926458090',
